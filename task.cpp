a2b4b2a44091cb3c3fdbda3fbfa224bd163b294c
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-03 23:28:07 +0300

    task 1 is done

--- task.cpp
+++ task.cpp
@@ -1,10 +1,19 @@
 // Вариант 18
 
 #include <iostream>
-
+#include <cmath>
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    double x;
+    std::cout << "Input x: " << std::endl;
+    std::cin >> x;
+
+    // По замечаниям к условиям задачи считаем, что вводимое значение принадлежит 
+    // области определения, поэтому проверку на деление на 0 не производим
+    double result = 2 / std::tan(3 * x) - 1 / (12 * x * x + 7 * x -5);
+
+    std::cout << "Result: " << result << std::endl;
+
     return 0;
 }
