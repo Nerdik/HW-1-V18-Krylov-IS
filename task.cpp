3e6cfdc6dd92789a7862f3f3c4e665f45be3bb97
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-03 23:38:25 +0300

    task 2 is done

--- task.cpp
+++ task.cpp
@@ -5,15 +5,28 @@
 
 int main()
 {
-    double x;
-    std::cout << "Input x: " << std::endl;
-    std::cin >> x;
+    // Задача 1. Вычислить значение выражения
+    // double x;
+    // std::cout << "Input x: " << std::endl;
+    // std::cin >> x;
 
-    // По замечаниям к условиям задачи считаем, что вводимое значение принадлежит 
-    // области определения, поэтому проверку на деление на 0 не производим
-    double result = 2 / std::tan(3 * x) - 1 / (12 * x * x + 7 * x -5);
+    // // По замечаниям к условиям задачи считаем, что вводимое значение принадлежит 
+    // // области определения, поэтому проверку на деление на 0 не производим
+    // double result = 2 / std::tan(3 * x) - 1 / (12 * x * x + 7 * x -5);
 
-    std::cout << "Result: " << result << std::endl;
+    // std::cout << "Result: " << result << std::endl;
+
+    // return 0;
+
+    
+    // Задача 2. Вычислить радиус окружности описанной около равностороннего треугольника со стороной a
+    double a;
+    std::cout << "Input a: " << std::endl;
+    std::cin >> a;
+
+    double radius = a / std::sqrt(3);
+
+    std::cout << "Radius: " << radius << std::endl;
 
     return 0;
 }
