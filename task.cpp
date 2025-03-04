3cda7e765bceaab128447009b024fc0feb73c026
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-04 13:59:26 +0300

    exercises 1 and 2 are made with functions

--- task.cpp
+++ task.cpp
@@ -1,32 +1,43 @@
-// Вариант 18
+// Var 18
 
 #include <iostream>
 #include <cmath>
 
-int main()
+// Function for Exercise 1.
+double ex1(double x)
+{    
+    double result = 2 / std::tan(3 * x) - 1 / (12 * x * x + 7 * x -5);
+
+    return result;
+}
+
+// Function for Exercise 2.
+double ex2(double a)
 {
-    // Задача 1. Вычислить значение выражения
-    // double x;
-    // std::cout << "Input x: " << std::endl;
-    // std::cin >> x;
+    double radius = a / std::sqrt(3);
 
-    // // По замечаниям к условиям задачи считаем, что вводимое значение принадлежит 
-    // // области определения, поэтому проверку на деление на 0 не производим
-    // double result = 2 / std::tan(3 * x) - 1 / (12 * x * x + 7 * x -5);
+    return radius;
+}
 
-    // std::cout << "Result: " << result << std::endl;
 
-    // return 0;
+int main()
+{   
+    // Exercise 1.
+    double x;
+    std::cout << "Exercise 1" << std::endl;
+    std::cout << "Input x: " << std::endl;
+    std::cin >> x;
+
+    std::cout << "Result: " << ex1(x) << std::endl;
 
     
-    // Задача 2. Вычислить радиус окружности описанной около равностороннего треугольника со стороной a
+    // Exercise 2.
     double a;
+    std::cout << "Exercise 2" << std::endl;
     std::cout << "Input a: " << std::endl;
     std::cin >> a;
 
-    double radius = a / std::sqrt(3);
-
-    std::cout << "Radius: " << radius << std::endl;
+    std::cout << "Radius: " << ex2(a) << std::endl;
 
     return 0;
 }
